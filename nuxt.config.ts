729ed80d83b0942e17e52ae0bbdcaa3d46b10d76
Jeffrey Yao <hello@jyao.me>
2022-10-26 19:47:32 +1100

    style/feat: replace Trunk w/ ESLint + Prettier and add Vue rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
-  /*   tailwindcss: {
-    // @ts-ignore
-    config: {
-      plugins: [require("@tailwindcss/typography")],
-    },
-  }, */
 });
---
+++ pages/courseStub.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-full min-h-screen w-full flex-col items-center bg-gray-50 p-12">
+    <header class="prose mb-12 prose-headings:font-medium">
+      <h1 className="font-medium">Course: <span class="font-bold">Mastering Nuxt 3</span></h1>
+    </header>
+
+    <div class="flex w-full grow justify-center">
+      <aside class="prose mr-4 flex min-w-[20ch] flex-col rounded-md bg-white p-8">
+        <h3>Chapters</h3>
+        <!-- All the lessons for the course listed here -->
+      </aside>
+
+      <section class="prose grow rounded-md bg-red-50 p-12">
+        <h2>Lesson</h2>
+        <p>This is a lesson</p>
+      </section>
+    </div>
+  </div>
+</template>
